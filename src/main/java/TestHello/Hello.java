d9cdc9cec6591a779e934ac96c3ccaa254659366
Pavel <gh7777@yandex.ru>
2020-03-03 12:44:31 +0300

    Conflict Test2  1 mer 3

--- src/main/java/TestHello/Hello.java
+++ src/main/java/TestHello/Hello.java
@@ -2,10 +2,10 @@ package TestHello;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!tonton1");
-        System.out.println("Hello World!!!!!tonton2");
-        System.out.println("Hello World!!!!!tonton3");
-        System.out.println("Hello World!!!!!tonton4");
-        System.out.println("Hello World!!!!!tonton5");
+        System.out.println("Hello World!!!!!conflictq");
+        System.out.println("Hello World!!!!!conflicta");
+        System.out.println("Hello World!!!!!conflictz");
+        System.out.println("Hello World!!!!!conflictx");
+        System.out.println("Hello World!!!!!conflictc");
     }
 }
