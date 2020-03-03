407fcb242d0d1fe93428e609c3a25fdb985ea75d
Pavel <gh7777@yandex.ru>
2020-03-03 12:29:42 +0300

    Conflict Test2  1 mer

--- src/main/java/TestHello/Hello.java
+++ src/main/java/TestHello/Hello.java
@@ -2,6 +2,10 @@ package TestHello;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!tonton");
+        System.out.println("Hello World!!!!!tonton1");
+        System.out.println("Hello World!!!!!tonton2");
+        System.out.println("Hello World!!!!!tonton3");
+        System.out.println("Hello World!!!!!tonton4");
+        System.out.println("Hello World!!!!!tonton5");
     }
 }
